988187c4a51e65a4c23fb0c13fe97e42ca123f1a
p-wel <wojtowiczp97@gmail.com>
2023-11-05 20:27:11 +0100

    Init commit

---
+++ src/test/java/com/pwel/allegrotrader/AllegroTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwel.allegrotrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllegroTraderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
