ae1f0777ff1c46c20dc976c096fedea4b3d6b0f3
p-wel <wojtowiczp97@gmail.com>
2024-02-17 14:30:52 +0100

    feat(0000): add data.sql and gcp_deploy script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- data.sql
+
+INSERT INTO product (name, category, image) VALUES ('Szyba przednia Toyota', 'Motoryzacja', 'https://media.istockphoto.com/id/1150931120/pl/zdj%C4%99cie/ilustracja-3d-og%C3%B3lnego-kompaktowego-bia%C5%82ego-samochodu-widok-z-przodu.jpg?s=2048x2048&w=is&k=20&c=MAaPrTdvk4qkiDA5Bu3oU2BedxetsuAPsr372KZg73Q=');
