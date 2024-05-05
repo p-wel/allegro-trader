8fb150301455fbe19e0cbb3fe15a5186d899d380
p-wel <wojtowiczp97@gmail.com>
2024-05-05 18:50:05 +0200

    feat(0000): fix getOffers mapper

--- src/main/java/com/pwel/allegrotrader/api/finder/model/offer/FixedPrice.java
+++ src/main/java/com/pwel/allegrotrader/api/finder/model/offer/FixedPrice.java
@@ -1,8 +1,10 @@
 package com.pwel.allegrotrader.api.finder.model.offer;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class FixedPrice {
     String amount;
     String currency;
