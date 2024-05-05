42a2507e4cb4ff07a653c60c8ca120be99655afb
p-wel <wojtowiczp97@gmail.com>
2024-05-05 22:38:54 +0200

    feat(0000): create FinderScheduler

--- src/main/java/com/pwel/allegrotrader/AllegroTraderApplication.java
+++ src/main/java/com/pwel/allegrotrader/AllegroTraderApplication.java
@@ -3,9 +3,11 @@ package com.pwel.allegrotrader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.pwel.allegrotrader")
 @ConfigurationPropertiesScan
+@EnableScheduling
 public class AllegroTraderApplication {
 
     public static void main(String[] args) {
